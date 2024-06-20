cf5b89056a3825c163f6a94b6db4d98166658922
govindghosh0 <govindghosh0@gmail.com>
2024-06-20 14:51:45 +0000

    [BUILD] Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
-import HeroSection from "../components/HeroSection"
+
+import HeroSection from "../components/HeroSection";
+import Footer from "@/components/Footer";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-[#282A36] text-[#f8f8f2]">
       <HeroSection />
+      <Footer />
     </main>
   );
 }
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,14 +7,23 @@ export default function HeroSection() {
   return (
     <div className="min-h-screen relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
       <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
- 
+
       <Boxes />
-      <h1 className={cn("md:text-4xl text-xl text-white relative z-20")}>
+      <h1 className={cn("md:text-4xl text-xl text-white relative z-20 pt-40")}>
         Tailwind is Awesome
       </h1>
       <p className="text-center mt-2 text-neutral-300 relative z-20">
         Framer motion is the best animation library ngl
       </p>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse ">
+          <img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+          <div>
+            <h1 className="text-5xl font-bold z-20 relative">Box Office News!</h1>
+            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
