73b70bd0c1ee67cc61a4cf7c451607761f9063fa
govindghosh0 <govindghosh0@gmail.com>
2024-06-21 06:41:43 +0000

    [FIX]HeroSection

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,28 +2,36 @@
 import React from "react";
 import { Boxes } from "./ui/background-boxes";
 import { cn } from "@/lib/utils";
+import { TextGenerateEffect } from "./ui/text-generate-effect";
+
+
 
 export default function HeroSection() {
   return (
     <div className="min-h-screen relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
       <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
-
       <Boxes />
-      <h1 className={cn("md:text-4xl text-xl text-white relative z-20 pt-40")}>
-        Tailwind is Awesome
-      </h1>
-      <p className="text-center mt-2 text-neutral-300 relative z-20">
-        Framer motion is the best animation library ngl
-      </p>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse ">
-          <img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
-          <div>
-            <h1 className="text-5xl font-bold z-20 relative">Box Office News!</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <div className="z-10 mb-10 flex justify-center">
+        <div className="flex max-w-[89vw] flex-col items-center justify-center md:max-w-2xl lg:max-w-[60vw]">
+          <h1 className={cn("md:text-6xl text-4xl text-white relative z-20 pt-36 pb-12")}>
+            Govind&apos;s Portfolio
+          </h1>
+          <div className="hero flex items-center justify-center">
+            <div className="hero-content flex flex-col items-center text-center">
+            {/* <img src="https://i.pinimg.com/564x/26/de/73/26de73409111ea0df3430f3d63e9bdc9.jpg" alt="govind_image" className="h-56 rounded-lg shadow-2xl" /> */}
+              <div className="w-11/12 sm:w-8/12">
+                <TextGenerateEffect 
+                className="text-center text-[40px] md:text-5xl lg:text-6xl text-4xl font-bold z-20 dark:text-neutral-300 text-neutral-300 relative" 
+                words="Turning Designs into Interactive Digital Realities" />
+                <TextGenerateEffect 
+                className="py-6 relative z-20 text-base sm:text-lg text-neutral-300 dark:text-neutral-300" 
+                words="Specializing in React, Redux, Next.js, Tailwind CSS, MongoDB, Node.js, and Express.js. Crafting dynamic, user-focused web applications." />
+              </div>
+            </div>
           </div>
         </div>
       </div>
+
     </div>
   )
 }
