bb5e88ad9d9993d57d707f0c2267aeb5b4255417
Govind ghosh <govindghosh0@gmail.com>
2024-06-22 18:05:48 +0530

    [FIX]darkmode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,13 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-
+import {ThemeProvider } from "@/components/ui/theme-provider"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Portfolio | Govind Ghosh",
-  description: "Welcome to my portfolio! I am a dedicated frontend developer with a strong proficiency in HTML, CSS, Tailwind, Bootstrap, JavaScript, React, Redux, and Next.js. My backend skills include MongoDB, Express.js, Node.js, Firebase, and Appwrite. I graduated from Dr. Bhim Rao Ambedkar University, and I am passionate about creating seamless user experiences and efficient web applications. Explore my projects and connect with me on GitHub at https://github.com/Govindghosh, LinkedIn at https://www.linkedin.com/in/govind-web-developer/, X at https://twitter.com/govind_ghosh, and Instagram at https://www.instagram.com/govindghosh0/.",
+  description:
+    "Welcome to my portfolio! I am a dedicated frontend developer with a strong proficiency in HTML, CSS, Tailwind, Bootstrap, JavaScript, React, Redux, and Next.js. My backend skills include MongoDB, Express.js, Node.js, Firebase, and Appwrite. I graduated from Dr. Bhim Rao Ambedkar University, and I am passionate about creating seamless user experiences and efficient web applications. Explore my projects and connect with me on GitHub at https://github.com/Govindghosh, LinkedIn at https://www.linkedin.com/in/govind-web-developer/, X at https://twitter.com/govind_ghosh, and Instagram at https://www.instagram.com/govindghosh0/.",
 };
 
 export default function RootLayout({
@@ -18,32 +19,57 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="description" content="Welcome to my portfolio! I am a dedicated frontend developer with a strong proficiency in HTML, CSS, Tailwind, Bootstrap, JavaScript, React, Redux, and Next.js. My backend skills include MongoDB, Express.js, Node.js, Firebase, and Appwrite. I graduated from Dr. Bhim Rao Ambedkar University, and I am passionate about creating seamless user experiences and efficient web applications. Explore my projects and connect with me on GitHub at https://github.com/Govindghosh, LinkedIn at https://www.linkedin.com/in/govind-web-developer/, X at https://twitter.com/govind_ghosh, and Instagram at https://www.instagram.com/govindghosh0/." />
-        
+        <meta
+          name="description"
+          content="Welcome to my portfolio! I am a dedicated frontend developer with a strong proficiency in HTML, CSS, Tailwind, Bootstrap, JavaScript, React, Redux, and Next.js. My backend skills include MongoDB, Express.js, Node.js, Firebase, and Appwrite. I graduated from Dr. Bhim Rao Ambedkar University, and I am passionate about creating seamless user experiences and efficient web applications. Explore my projects and connect with me on GitHub at https://github.com/Govindghosh, LinkedIn at https://www.linkedin.com/in/govind-web-developer/, X at https://twitter.com/govind_ghosh, and Instagram at https://www.instagram.com/govindghosh0/."
+        />
+
         {/* Open Graph / Facebook */}
         <meta property="og:title" content="Portfolio | Govind Ghosh" />
-        <meta property="og:description" content="Welcome to my portfolio! I am a dedicated frontend developer with a strong proficiency in HTML, CSS, Tailwind, Bootstrap, JavaScript, React, Redux, and Next.js. My backend skills include MongoDB, Express.js, Node.js, Firebase, and Appwrite." />
-        <meta property="og:url" content="https://www.instagram.com/govindghosh0/" />
+        <meta
+          property="og:description"
+          content="Welcome to my portfolio! I am a dedicated frontend developer with a strong proficiency in HTML, CSS, Tailwind, Bootstrap, JavaScript, React, Redux, and Next.js. My backend skills include MongoDB, Express.js, Node.js, Firebase, and Appwrite."
+        />
+        <meta
+          property="og:url"
+          content="https://www.instagram.com/govindghosh0/"
+        />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.instagram.com/govindghosh0/" />
-        
-         {/* Twitter  */}
+        <meta
+          property="og:image"
+          content="https://www.instagram.com/govindghosh0/"
+        />
+
+        {/* Twitter  */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Portfolio | Govind Ghosh" />
-        <meta name="twitter:description" content="Welcome to my portfolio! I am a dedicated frontend developer with a strong proficiency in HTML, CSS, Tailwind, Bootstrap, JavaScript, React, Redux, and Next.js. My backend skills include MongoDB, Express.js, Node.js, Firebase, and Appwrite." />
+        <meta
+          name="twitter:description"
+          content="Welcome to my portfolio! I am a dedicated frontend developer with a strong proficiency in HTML, CSS, Tailwind, Bootstrap, JavaScript, React, Redux, and Next.js. My backend skills include MongoDB, Express.js, Node.js, Firebase, and Appwrite."
+        />
         <meta name="twitter:image" content="https://x.com/govind_ghosh/photo" />
         <meta name="twitter:site" content="@govind_ghosh" />
-        
+
         {/* Additional SEO */}
-        <meta name="keywords" content="Frontend Developer, HTML, CSS, Tailwind, Bootstrap, JavaScript, React, Redux, Next.js, MongoDB, Express.js, Node.js, Firebase, Appwrite, Portfolio, Govind Ghosh" />
+        <meta
+          name="keywords"
+          content="Frontend Developer, HTML, CSS, Tailwind, Bootstrap, JavaScript, React, Redux, Next.js, MongoDB, Express.js, Node.js, Firebase, Appwrite, Portfolio, Govind Ghosh"
+        />
         <meta name="author" content="Govind Ghosh" />
         <link rel="canonical" href="https://your-portfolio-website.com" />
-        
+
         <title>Portfolio | Govind Ghosh</title>
       </head>
       <body className={inter.className}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
         <Navbar />
         {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,12 +1,22 @@
-"use client"
-import React, { useState } from 'react'
+"use client";
+import React, { useState } from "react";
 
 export default function Project() {
-    const [expandedId, setExpandedId] = useState<number | null>(null);
-    const handleExpandClick = (id: number) => {
-        setExpandedId(expandedId === id ? null : id);
-      };
+  const [expandedId, setExpandedId] = useState<number | null>(null);
+
+  const handleExpandClick = (id: number) => {
+    setExpandedId(expandedId === id ? null : id);
+  };
+
+
+
   return (
-    <div>Featured Projects</div>
-  )
+    <div className="h-[40rem] w-full dark:bg-[#0f172a] bg-[#f8f8f2]  dark:bg-dot-sky/[0.2] bg-dot-white/[0.5] relative flex items-center justify-center">
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-[#0f172a] bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,white)]"></div>
+      <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+        Backgrounds
+      </p>
+    </div>
+  );
 }
