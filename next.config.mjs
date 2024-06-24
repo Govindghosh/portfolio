2f83f40da2d5f35e62361086db62c8bead04fd1a
Govind ghosh <govindghosh0@gmail.com>
2024-06-24 14:45:44 +0530

    [BUILD]Technologival BentoGrid

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['assets.aceternity.com',"i.pinimg.com" ],
+        domains: ['assets.aceternity.com',
+                  'i.pinimg.com', 
+                  'pbs.twimg.com' ],
       },
 };
 
